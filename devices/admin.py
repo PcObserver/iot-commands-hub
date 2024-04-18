67a81d7d0b50a5123ed4bf2114b6b78f688bf213
dgs-small <lucas9667@hotmail.com>
2024-04-18 17:58:32 -0300

    Created admin sections for devices models

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Device, Action
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_name', 'perma_name', 'prefix',)
+
+    
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_name', 'perma_name', 'brand',)
+
+    
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'device',)    
