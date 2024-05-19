4faba295df702cbec09f9a81307509c01b157984
dgs-small <lucas9667@hotmail.com>
2024-05-19 18:29:52 -0300

    Created new GET endpoint to fetch users' brands, devices or actions

--- iot_commands_hub/settings.py
+++ iot_commands_hub/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 30,
+    "PAGE_SIZE": 10,
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
