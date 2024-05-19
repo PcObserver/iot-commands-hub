4d0af278cc8a5f42dda67aea7dc428c50cf10581
dgs-small <lucas9667@hotmail.com>
2024-05-19 16:55:59 -0300

    Ordering contributions by positive reviews by default

--- devices/apiViews.py
+++ devices/apiViews.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Count, Q
 
 from .models import Brand, Device, Action
 from .serializers import BrandSerializer, DeviceSerializer, ActionSerializer
@@ -11,12 +12,26 @@ class BrandViewSet(ModelViewSet):
     filterset_class = BrandFilter
     http_method_names = ["get", "patch"]
 
+    def get_queryset(self):
+        queryset = self.queryset.annotate(
+            positive_reviews_count=Count("reviews", filter=Q(reviews__type=0))
+        ).order_by("-positive_reviews_count")
+
+        return queryset
+
 
 class DeviceViewSet(ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
     filterset_class = DeviceFilter
     http_method_names = ["get", "patch"]
+    
+    def get_queryset(self):
+        queryset = self.queryset.annotate(
+            positive_reviews_count=Count("reviews", filter=Q(reviews__type=0))
+        ).order_by("-positive_reviews_count")
+
+        return queryset
 
 
 class ActionViewSet(ModelViewSet):
@@ -24,3 +39,10 @@ class ActionViewSet(ModelViewSet):
     serializer_class = ActionSerializer
     filterset_class = ActionFilter
     http_method_names = ["get", "patch"]
+    
+    def get_queryset(self):
+        queryset = self.queryset.annotate(
+            positive_reviews_count=Count("reviews", filter=Q(reviews__type=0))
+        ).order_by("-positive_reviews_count")
+
+        return queryset
